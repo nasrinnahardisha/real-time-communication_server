10695ae7ba9c12c60ebfa4abdc92de0abd8ddf7c
NasrinNaharDisha <nasrinnahardisha@gmail.com>
2026-09-07 00:29:03 +0600

    fix cors array and socket origin syntax

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "https://real-time-communication-client.onrender.com",
+      "https://real-time-communicationclient.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:3000",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   },
@@ -86,7 +91,12 @@ initializeApp({
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "https://real-time-communication-client.onrender.com",
+      "https://real-time-communicationclient.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:3000",
+    ],
     credentials: true,
   }),
 );
